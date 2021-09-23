96d4ef222817d00a28014c4fa9e3c67e6b46668b
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-09-23 11:15:00 +0530

    added npm package

--- Toggler.js
+++ Toggler.js
@@ -1,28 +1,32 @@
 var isEvenCount
 var count = 1
 
-function InteractionCount() {
-    count++
-    isEvenCount = (count % 2 == 0)
+function Toggler() {
+    function InteractionCount() {
+        count++
+        isEvenCount = (count % 2 == 0)
+    }
+
+    function toggleClass(elementId, preClass, aftClass) {
+        var el = document.getElementById(`${elementId}`)
+        if (isEvenCount) {
+            el.className = el.className.replace(`${preClass}`, `${aftClass}`);
+        }
+        else {
+            el.className = el.className.replace(`${aftClass}`, `${preClass}`);
+        }
+    }
+
+    function toggleImage(elementId, fromImg, toImg) {
+        var el = document.getElementById(`${elementId}`)
+        if (isEvenCount) {
+            el.src = `${toImg}`
+            console.log(`image source set to ${toImg}`)
+        } else {
+            el.src = `${fromImg}`
+            console.log(`image source set to ${fromImg}`)
+        }
+    }
 }
 
-function toggleClass(elementId, preClass, aftClass) {
-    var el = document.getElementById(`${elementId}`)
-    if (isEvenCount) {
-        el.className = el.className.replace(`${preClass}`, `${aftClass}`);
-    }
-    else {
-        el.className = el.className.replace(`${aftClass}`, `${preClass}`);
-    }
-}
-
-function toggleImage(elementId, fromImg, toImg) {
-    var el = document.getElementById(`${elementId}`)
-    if (isEvenCount) {
-        el.src = `${toImg}`
-        console.log(`image source set to ${toImg}`)
-    } else {
-        el.src = `${fromImg}`
-        console.log(`image source set to ${fromImg}`)
-    }
-}
+module.exports.Toggler = Toggler
