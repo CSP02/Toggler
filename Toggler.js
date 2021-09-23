86abc4dae655753f9aad974a0df4e625482b4c33
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-09-23 15:39:55 +0530

    updated readme

--- Toggler.js
+++ Toggler.js
@@ -18,4 +18,39 @@ class Toggler {
             console.log(`image source set to ${fromImg}`)
         }
     }
+
+
+    //ToggleSlide which will toggles the slide or an element in given direction
+    toggleSlide(elementId, direction, speed) {
+        var el = document.getElementById(`${elementId}`)
+        var elWidth = el.offsetWidth
+        var elHeight = el.offsetHeight
+        var offsetLeft = el.offsetLeft
+        var addPos = offsetLeft
+        if (direction == 'horizontal') {
+            if (offsetLeft > -elWidth)
+                var toggle = setInterval(fadeHorizontal, speed)
+            else if (offsetLeft <= -elWidth)
+                var toggle = setInterval(fadeOutHorizontal, speed)
+        } else if (direction == 'vertical') {
+            //still in dev
+        }
+        function fadeHorizontal() {
+            if (offsetLeft <= -elWidth)
+                clearInterval(toggle)
+            else {
+                console.log(offsetLeft)
+                offsetLeft -= 10;
+                el.style.left = offsetLeft + 'px';
+            }
+        }
+        function fadeOutHorizontal() {
+            if (offsetLeft >= elWidth + addPos)
+                clearInterval(toggle)
+            else {
+                offsetLeft += 10;
+                el.style.left = offsetLeft + 'px';
+            }
+        }
+    }
 }
