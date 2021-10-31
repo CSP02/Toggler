6a1376097a628914f572089dd8e9379039d08e9e
chandu-programmer <65494930+Chandra-sekhar-pilla@users.noreply.github.com>
2021-10-31 11:30:44 +0530

    Added error handling for toggleClass() {experimental feature}

--- Toggler.js
+++ Toggler.js
@@ -1,25 +1,47 @@
 class Toggler {
     //toggleClass() will toggle the provided class name and the current class name. Works for multiple classes too
     toggleClass(elementId, preClass, aftClass) {
-        var element = document.getElementById(elementId)
-        const isCCGO = element.className.split(" ").length > 1 //isCCGO stands for isClassCountGreaterThanOne
-        if (!isCCGO) {
-            if (element.className == preClass) {
-                element.className = aftClass
-            } else if (element.className == aftClass) {
-                element.className = preClass
+        try {
+            if (elementId == null) {
+                throw new SyntaxError('ElementID was not provided')
+            } else if (preClass == null) {
+                throw new SyntaxError('(preClass is not provided) Previous class or current class is not provided.')
+            } else if (aftClass == null) {
+                throw new SyntaxError('(aftClass is not provided) Provide a string of class name that toggler has to toggle between.')
+            } else if (!isNaN(elementId) || !isNaN(preClass) || !isNaN(aftClass)) {
+                throw new TypeError('All the parameters are strings. Check if you passed the string parameters.')
             }
-        } else if (isCCGO) {
-            let replaced = element.className.split(' ')
-            replaced.forEach(value => {
-                if (value == preClass) {
-                    let replacedVal = element.className.replace(value, '')
-                    element.className = `${replacedVal}  ${aftClass}`
-                } else if (value == aftClass) {
-                    let replacedVal = element.className.replace(value, '')
-                    element.className = `${replacedVal}  ${preClass}`
+            let element = document.getElementById(elementId)
+            const isCCGO = element.className.split(" ").length > 1 //isCCGO stands for isClassCountGreaterThanOne
+            if (!isCCGO) {
+                if (element.className == preClass) {
+                    element.className = aftClass
+                } else if (element.className == aftClass) {
+                    element.className = preClass
                 }
-            })
+            } else if (isCCGO) {
+                let replaced = element.className.split(' ')
+                replaced.forEach(value => {
+                    if (value == preClass) {
+                        let replacedVal = element.className.replace(value, '')
+                        element.className = `${replacedVal}  ${aftClass}`
+                    } else if (value == aftClass) {
+                        let replacedVal = element.className.replace(value, '')
+                        element.className = `${replacedVal}  ${preClass}`
+                    }
+                })
+            }
+        } catch (e) {
+            if (e instanceof TypeError && e.message.includes('null'))
+                e = 'Provided ID was not found check if there is a typo in the parameters passed.\n'
+            const dialog = document.createElement('dialog')
+            dialog.className = "CSP'sDialogClass"
+            dialog.id = "CSP'sDialogClass"
+            dialog.style = 'height:auto;width:300px;margin:-50% auto auto 30%;padding: 3%;font-size: 18px;text-align: center;text- ustify: initial;color: rgb(221, 221, 221);background-color: rgba(37, 37, 37, 0.6);backdrop-filter: blur(15px);border:none;border-radius:6px;float:right;'
+            this.errorHandling(dialog, e)
+            setTimeout(() => {
+                dialog.remove()
+            }, 10000)
         }
     }
 
@@ -99,4 +121,17 @@ class Toggler {
         if (element.style.getPropertyValue(property) == fromValue) element.style.setProperty(property, toValue)
         else element.style.setProperty(property, fromValue)
     }
+
+    errorHandling = function (dialog, e) {
+        document.body.appendChild(dialog)
+        let splitted = e.stack.split('\n')
+        splitted = splitted[2].split('at')
+        dialog.innerHTML += `${e.message}<br><span style="color:red;">at ${splitted[1]}</span><br><button id="CSP'scloseDialog" onclick="TogglerCloseDialog()">Close</button>`
+        dialog.open = true
+    }
+}
+
+function TogglerCloseDialog() {
+    let dialogs = document.getElementById("CSP'sDialogClass")
+    dialogs.remove()
 }
